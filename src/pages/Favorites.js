1515cefe12d01200507c1886f7a46b458e402b70
Aytuhan Kazandirmak <aytuhann@gmail.com>
2024-01-02 22:17:13 +0300

    update

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -5,10 +5,14 @@ import { MyAuthContext } from "../component/context/ContextAuth";
 
 const FavoritesPage = () => {
   const { user } = useContext(MyAuthContext);
+
   return (
     <div>
       <Layouts login="login">
-        <h3>{user.displayName}'s Favorites </h3>
+        <h3>
+          {user.displayName}'s {user.email === "ssezen2001@gmail.com" && "❤️ "}
+          Favorites
+        </h3>
         <FavoritesList />
       </Layouts>
     </div>
